ea1c33b60ab04a896ec88e720f7406467381e97c
Denise Rezende <denise.rezende@gmail.com>
2023-11-17 11:25:24 -0300

    Ajustando heartbeat e sistema de falhas

--- ProjetoMulticast/main.py
+++ ProjetoMulticast/main.py
@@ -6,6 +6,23 @@ import logging
 import os
 import datetime
 
+# timeout poderia ajustar automatico
+# ajustar para dois delta t - basicamente min = 1 s
+# reajusta por cliente 
+# reajuste para mais tempo caso seja maior
+# timeout por cliente
+# timeout do cliente sempre 0.5 OK
+
+# menu 
+# setar atraso artificial + 0.5 do cliente - para esse tempo depois responde que recebeu - simular um problema
+# enviar mensagem
+
+#acknowledge junto com o delta que demorou pra responder ok
+
+# o heartbeat desliga ele da sua lista e religa, então tem que ficar mandando para eles ok
+
+
+
 # Configurações do grupo multicast
 MULTICAST_GROUP = '224.1.1.1'
 MULTICAST_PORT = 5007
@@ -17,7 +34,8 @@ host_ports = {}
 with open('multicast.txt', 'r') as file:
     for line in file:
         ip, port, host = line.strip().split()
-        host_ports[host] = (ip, int(port))
+        # (x,y, 1 é online e 0 é offline, timeout)
+        host_ports[host] = (ip, int(port), 0, 2) 
 
 print(host_ports)
 
@@ -57,23 +75,18 @@ def config_socket():
 
 
 sock = config_socket()
-online_hosts = []
-offline_hosts = []
 file_content = []
 
 
 def add_online_host(host):
-    if host not in online_hosts:
-        online_hosts.append(host)
-        if host in offline_hosts:
-            offline_hosts.remove(host)
-
+    host_ports[host] = host_ports[host][:2] + (1,) + host_ports[host][3:] # Para mudar para 1 (online)
 
 def remove_online_host(host):
-    if host not in offline_hosts:
-        offline_hosts.append(host)
-        if host in online_hosts:
-            online_hosts.remove(host)
+    host_ports[host] = host_ports[host][:2] + (0,)  + host_ports[host][3:] # Para mudar para 0 (offline)
+
+def change_timeout_host(host, new_timeout):
+    host_ports[host] = host_ports[host][:3] + (new_timeout,)
+
 
 
 global_amount = 0
@@ -81,72 +94,102 @@ global_amount = 0
 
 def _send_message_(message, host, address):
     ip, port = address
+    await_time = host_ports[host][-1]*2
+    print(f"timeout={await_time/2}")
     try:
+        start_time = time.time()
         print(f'Sending "{message}" to ({ip}, {port})')
         sock.sendto(message.encode(), (ip, port))
-        sock.settimeout(TIMEOUT)
+        sock.settimeout(await_time)
         response, address = sock.recvfrom(1024)
         decoded_response = response.decode()
         if decoded_response == 'acknowledge':
-            print("Acknowledged")
-            add_online_host(port)
+            end_time = time.time()
+            print(f"Acknowledged {end_time-start_time}")
+            change_timeout_host(host, end_time-start_time)
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             file_content.append(f'{timestamp} || Mensagem: "{message}" enviada para {ip}, {port}')
         else:
             # Manda a mensagem novamente caso não tiver sido recebida
             _send_message_(message, host, address)
     except socket.timeout:
-        print(f"Timeout: A mensagem não foi recebida em {TIMEOUT} segundos.")
+        change_timeout_host(host, await_time * 2)
+        # print(f"Timeout: A mensagem não foi recebida em {TIMEOUT} segundos.")
         # Manda a mensagem novamente caso tenha ocorrido timeout
         global global_amount
         global_amount += 1
-        if global_amount < 3:
+        if global_amount < 2:
             _send_message_(message, host, address)
         global_amount = 0
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file_content.append(f'{timestamp} || Mensagem: "{message}" não enviada para {ip}, {port}')
     except Exception as e:
-        logging.warning(f"Error: {e}")
+        pass
+        # logging.warning(f"Error: {e}")
 
 
 # Função para enviar mensagens
 def send_message(message):
+    offline_hosts = []
     for host, address in host_ports.items():
-        _send_message_(message, host, address)
-
+        ip, port, status, timeout = address  
+        if status == 1:
+            _send_message_(message, host, address[:2])
+        else:
+            offline_hosts.append(host)
+    for h in offline_hosts:
+        print(f"Host: {h} seems to be offline.")
 
 # Função para receber mensagens
 def receive_message():
+    elapsed_time = 1
+    last_heartbeat = time.time()  # Defina last_heartbeat antes do loop
+    key = None
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.bind(current_address_info)
+        sock.bind(current_address_info[:2]) 
+        sock.settimeout(5) # Configura um timeout de 5 segundos para ser esse o limite do heartbeat
         while True:
-            data, address = sock.recvfrom(1024)
-            ip, port = address
-            decoded_data = data.decode()
-            if decoded_data == 'heartbeat':
-                add_online_host(port)
-            else:
-                print(f'Received "{decoded_data}" from ({ip}, {port})')
-                sock.sendto("acknowledge".encode(), (ip, port))  # MULTICAST_PORT))
-                add_online_host(port)
-                print(f"Digite uma mensagem: ")
+            try:
+                data, address = sock.recvfrom(1024)
+                ip, port = address
+                decoded_data = data.decode()
+                # Separar a string usando '/'
+                parts = decoded_data.split('/')
+                # Obter a segunda parte (o host)
+                message = parts[0]
+                if message == 'heartbeat':
+                    key = parts[1]
+                    add_online_host(key)
+                    last_heartbeat = time.time()
+                else:
+                    print(f'Received "{message}" from ({ip}, {port})')
+                    sock.sendto("acknowledge".encode(), (ip, port))  # MULTICAST_PORT))
+                    print(f"Digite uma mensagem: ")
+            except Exception as e:
+                elapsed_time = time.time() - last_heartbeat
+                if elapsed_time > 5 and key != None:
+                    remove_online_host(key)
+                    key = None
 
 
 # Função para enviar heartbeat
 def send_message_heartbeat():
-    message = "heartbeat"
     while True:
         for host, address in host_ports.items():
-            ip, port = address
+            message = f"heartbeat/{host_name}"
+            ip, port, status, timeout = address
             try:
                 logging.info(f'Sending "heartbeat" to ({ip}, {port})')
-                sock.sendto("heartbeat".encode(), (ip, port))
+                sock.sendto(message.encode(), (ip, port))
                 sock.settimeout(TIMEOUT)
             except socket.timeout:
-                logging.warning(f"({ip}, {port}) seems to be offline.")
-                remove_online_host(port)
+                logging.warning(f"{host}: ({ip}, {port}) seems to be offline.")
+                remove_online_host(host)
             except Exception as e:
-                logging.warning(f"Error: {e}")
+                remove_online_host(host)
+                # desligar o host
+                pass
+                # logging.warning(f"Error: {e}")
         time.sleep(2)  # Espera 20 segundos antes de enviar o próximo heartbeat
 
 
